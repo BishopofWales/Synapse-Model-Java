01402c0bb4c4349a6c03c53dfc6e76b2f9dc04b3
Michael Hermenault <mhermenault59@gmail.com>
2019-01-05 20:02:28 -0500

    quick commit to edit elsewhere

---
+++ Lizard.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Lizard {
+    public static final double LIN_SPD = 5.0; // m/s
+    public static final double ANG_SPD = Math.PI / 2.0; // rad/s
+    public static final int EYE_NEUR = 50;
+    public static final int RIGHT_NEUR = 51;
+    public static final int LEFT_NEUR = 52;
+    public static final int FORWARD_NEUR = 53;
+
+    private Brain _lizBrain;
+    private double _xPos;
+    private double _yPos;
+    private double _rotation;
+    private double _score;
+
+    public Lizard(double xPos, double yPos, double rotation, Random rand) {
+        _lizBrain = new Brain(rand);
+        _xPos = xPos;
+        _yPos = yPos;
+        _rotation = rotation;
+    }
+
+    void proccessInput(Circle[] worldGeom, long time) {
+        // First we check if we see any geometry and stimulate the eye neuron if we do.
+        for (Circle circle : worldGeom) {
+            if (Raycast.circleRayCol(circle.getRadius(), circle.getX(), circle.getY(), _xPos, _yPos, _rotation)) {
+                _lizBrain.stimulateNeur(EYE_NEUR, time);
+            }
+        }
+        // Next we check to see if the muscle neurons have fired and move the creature
+        // accordingly.
+        if (_lizBrain.readNeur(LEFT_NEUR)) {
+            _rotation -= ANG_SPD;
+        }
+        if (_lizBrain.readNeur(RIGHT_NEUR)) {
+            _rotation += ANG_SPD;
+        }
+        if (_lizBrain.readNeur(FORWARD_NEUR)) {
+            _xPos += Math.cos(_rotation) * LIN_SPD;
+            _yPos += Math.sin(_rotation) * LIN_SPD;
+        }
+    }
+
+    public double getX() {
+        return _xPos;
+    }
+
+    public double getY() {
+        return _yPos;
+    }
+
+    public double getScore() {
+        return _score;
+    }
+
+    public void setScore(double score) {
+        _score = score;
+    }
+}
--- Raycast.java
+++ Raycast.java
@@ -1,37 +1,38 @@
-import java.util.*;
 
-public class Raycast{
-    public static boolean circleRayCol(double radius, double circ_x, double circ_y, double rayStart_x, double rayStart_y, double rayAngle)
-    {
-        //Calculate the slope of the ray line.
+public class Raycast {
+    public static boolean circleRayCol(double radius, double circ_x, double circ_y, double rayStart_x,
+            double rayStart_y, double rayAngle) {
+        // Calculate the slope of the ray line.
         double raySlope = Math.sin(rayAngle) / Math.cos(rayAngle);
-        //Calculate the origin of the ray line.
+        // Calculate the origin of the ray line.
         double rayOrigin = getOrigin(rayStart_x, rayStart_y, raySlope);
 
-        //Calculate normal of ray angle
+        // Calculate normal of ray angle
         double normal = rayAngle + Math.PI / 2;
-        //Calculate the slope of the normal line from the ray to the circle.
+        // Calculate the slope of the normal line from the ray to the circle.
         double normalSlope = Math.sin(normal) / Math.cos(normal);
-        //Calculate the y-origin of the normal line from the ray to the circle.
+        // Calculate the y-origin of the normal line from the ray to the circle.
         double normalOrigin = getOrigin(circ_x, circ_y, normalSlope);
 
-        //Find where the two lines collide
+        // Find where the two lines collide
         double col_y = ylineCol(raySlope, normalSlope, rayOrigin, normalOrigin);
         double col_x = (col_y - normalOrigin) / normalSlope;
 
-        //compare distance from origin to colision to the radius of the circle AND make sure the collision occurs along the ray, not behind it.
-        return Math.pow(col_x - circ_x, 2) + Math.pow(col_y - circ_y, 2) < Math.pow(radius, 2) && ((Math.cos(rayAngle) > 0) == (circ_x > rayStart_x));
+        // compare distance from origin to colision to the radius of the circle AND make
+        // sure the collision occurs along the ray, not behind it.
+        return Math.pow(col_x - circ_x, 2) + Math.pow(col_y - circ_y, 2) < Math.pow(radius, 2)
+                && ((Math.cos(rayAngle) > 0) == (circ_x > rayStart_x));
     }
-    private static double ylineCol(double m1, double m2, double b1, double b2)
-    {
+
+    private static double ylineCol(double m1, double m2, double b1, double b2) {
         return (m1 * b2 - m2 * b1) / (m1 - m2);
     }
-    private static double getOrigin(double x, double y, double m)
-    {
+
+    private static double getOrigin(double x, double y, double m) {
         return (y - m * x);
     }
-    public static double distSq(double x1, double y1, double x2, double y2)
-    {
+
+    public static double distSq(double x1, double y1, double x2, double y2) {
         double dx = x1 - x2;
         double dy = y1 - y2;
         return dx * dx + dy * dy;
